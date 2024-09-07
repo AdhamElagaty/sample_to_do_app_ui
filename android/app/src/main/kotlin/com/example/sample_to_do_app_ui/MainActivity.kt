6028c227ff1cc2615c21113cad74ec512634253a
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-08 02:46:21 +0300

    Create project file

---
+++ android/app/src/main/kotlin/com/example/sample_to_do_app_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sample_to_do_app_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SampleToDoAppUI());
+}
+
+class SampleToDoAppUI extends StatelessWidget {
+  const SampleToDoAppUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
