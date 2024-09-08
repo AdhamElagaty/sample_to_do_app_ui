56a02d25d31634e2d0ebbb9b7dee212aa23e3d04
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-08 04:00:26 +0300

    Edit on app style

--- lib/utils/app_style.dart
+++ lib/utils/app_style.dart
@@ -3,35 +3,30 @@ import 'package:flutter/material.dart';
 abstract class AppStyle {
   static const TextStyle styleBold22 = TextStyle(
     color: Color(0xff000000),
-    fontFamily: "SanFrancisco",
-    fontSize: 22,
-    fontWeight: FontWeight.bold,
+    fontSize: 36,
+    fontWeight: FontWeight.w800,
   );
 
   static const TextStyle styleRegular16 = TextStyle(
     color: Color(0xffA3A3A3),
-    fontFamily: "SanFrancisco",
-    fontSize: 16,
+    fontSize: 24,
     fontWeight: FontWeight.w400,
   );
 
   static const TextStyle styleBold18 = TextStyle(
     color: Color(0xff000000),
-    fontFamily: "SanFrancisco",
     fontSize: 18,
     fontWeight: FontWeight.bold,
   );
 
   static const TextStyle styleSemibold14 = TextStyle(
     color: Color(0xffF2994A),
-    fontFamily: "SanFrancisco",
     fontSize: 14,
     fontWeight: FontWeight.w600,
   );
 
   static const TextStyle styleRegular18 = TextStyle(
     color: Color(0xff000000),
-    fontFamily: "SanFrancisco",
     fontSize: 18,
     fontWeight: FontWeight.w400,
   );
