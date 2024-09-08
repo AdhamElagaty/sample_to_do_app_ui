f97001a52df9e076b20890809a6c260aadb4c291
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-08 03:07:17 +0300

    Create App Style Class

---
+++ lib/utils/app_style.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyle {
+  static const TextStyle styleBold22 = TextStyle(
+    color: Color(0xff000000),
+    fontFamily: "SanFrancisco",
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle styleRegular16 = TextStyle(
+    color: Color(0xffA3A3A3),
+    fontFamily: "SanFrancisco",
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleBold18 = TextStyle(
+    color: Color(0xff000000),
+    fontFamily: "SanFrancisco",
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle styleSemibold14 = TextStyle(
+    color: Color(0xffF2994A),
+    fontFamily: "SanFrancisco",
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleRegular18 = TextStyle(
+    color: Color(0xff000000),
+    fontFamily: "SanFrancisco",
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+  );
+}
