7eba2efda62fd44f06179424ffea8b86e5603822
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-12 22:37:12 +0300

    Update SubTaskWidget with updating UI and with provide methdes of update and delete

--- lib/widgets/sub_task_widget.dart
+++ lib/widgets/sub_task_widget.dart
@@ -4,6 +4,7 @@ import 'package:sample_to_do_app_ui/cubits/sub_task_cubit/sub_task_cubit.dart';
 import 'package:sample_to_do_app_ui/cubits/sub_task_cubit/sub_task_state.dart';
 import 'package:sample_to_do_app_ui/cubits/task_cubit/task_cubit.dart';
 import 'package:sample_to_do_app_ui/utils/app_style.dart';
+import 'package:sample_to_do_app_ui/widgets/edit_sub_task_form_dialog_body.dart';
 
 class SubTaskWidget extends StatelessWidget {
   const SubTaskWidget({
@@ -27,29 +28,73 @@ class SubTaskWidget extends StatelessWidget {
         }
       },
       builder: (context, state) {
-        return GestureDetector(
-          onTap: subTaskCubit.updateSubTaskCompletion,
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
           child: Container(
-            width: double.infinity,
-            color: Colors.transparent,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: const Color.fromARGB(255, 197, 192, 192),
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.circular(6)),
             child: Row(
-              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Checkbox(
-                  activeColor: const Color(0xff34C759),
-                  value: subTaskCubit.subTaskModel!.isCompleted,
-                  onChanged: (value) {
-                    subTaskCubit.updateSubTaskCompletion();
-                  },
+                Expanded(
+                  child: GestureDetector(
+                    onTap: subTaskCubit.updateSubTaskCompletion,
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Checkbox(
+                            activeColor: const Color(0xff34C759),
+                            value: subTaskCubit.subTaskModel!.isCompleted,
+                            onChanged: (value) {
+                              subTaskCubit.updateSubTaskCompletion();
+                            },
+                          ),
+                          Text(
+                            subTaskCubit.subTaskModel!.title,
+                            style: subTaskCubit.subTaskModel!.isCompleted
+                                ? AppStyle.styleRegular16.copyWith(
+                                    decoration: TextDecoration.lineThrough,
+                                  )
+                                : AppStyle.styleRegular16
+                                    .copyWith(color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-                Text(
-                  subTaskCubit.subTaskModel!.title,
-                  style: subTaskCubit.subTaskModel!.isCompleted
-                      ? AppStyle.styleRegular16.copyWith(
-                          decoration: TextDecoration.lineThrough,
-                        )
-                      : AppStyle.styleRegular16.copyWith(color: Colors.black),
-                ),
+                Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(
+                        Icons.edit,
+                        color: Color(0xfff19e00),
+                      ),
+                      onPressed: () {
+                        BlocProvider.of<SubTaskCubit>(context).updateSupTask(
+                          context,
+                          BlocProvider.value(
+                            value: BlocProvider.of<SubTaskCubit>(context),
+                            child: const EditSubTaskFormDialogBody(),
+                          ),
+                        );
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        BlocProvider.of<TaskCubit>(context)
+                            .deleteSupTask(subTaskIndex);
+                      },
+                    ),
+                  ],
+                )
               ],
             ),
           ),
