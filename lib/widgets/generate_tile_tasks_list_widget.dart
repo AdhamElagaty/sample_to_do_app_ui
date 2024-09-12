c8a8ff31ddd0d153fce3e3e9c8b2e09675ed48f7
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-12 17:22:58 +0300

    Update on GenerateTileTasksListWidget

--- lib/widgets/generate_tile_tasks_list_widget.dart
+++ lib/widgets/generate_tile_tasks_list_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sample_to_do_app_ui/cubits/task_cubit/task_cubit.dart';
 import 'package:sample_to_do_app_ui/cubits/tasks_view_cubit/tasks_view_cubit.dart';
 import 'package:sample_to_do_app_ui/cubits/tasks_view_cubit/tasks_view_state.dart';
+import 'package:sample_to_do_app_ui/models/task_model.dart';
 import 'package:sample_to_do_app_ui/utils/app_style.dart';
 import 'package:sample_to_do_app_ui/utils/helper.dart';
 import 'package:sample_to_do_app_ui/widgets/custom_expansion_tile_task_widget.dart';
@@ -18,17 +20,20 @@ class GenerateTileTasksListWidget extends StatelessWidget {
           return const Center(
             child: CircularProgressIndicator(),
           );
-        } else if (snapshot.hasError) {
-          return Center(
-            child: Text('Error: ${snapshot.error}'),
-          );
-        } else {
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          }
           return BlocConsumer<TasksViewCubit, TasksViewState>(
             listener: (context, state) {},
             builder: (context, state) {
               return tasksUIList(state, context);
             },
           );
+        } else {
+          return const Center(child: Text('Something went wrong.'));
         }
       },
     );
@@ -38,38 +43,39 @@ class GenerateTileTasksListWidget extends StatelessWidget {
     if (state is UpdateDataViewTasksSuccess) {
       final tasks = BlocProvider.of<TasksViewCubit>(context).tasksModel;
       if (tasks == null || tasks.isEmpty) {
-        return Center(
-          child: Text(
-            "No Tasks in this day 😊",
-            style: AppStyle.styleBold18
-                .copyWith(color: const Color(0xff000000).withOpacity(0.5)),
-          ),
-        );
+        return buildNoTasksMessage();
       }
-      return ListView.builder(
-        itemCount: tasks.length,
-        itemBuilder: (context, index) => CustomExpansionTileTaskWidget(
+      return buildTasksList(tasks);
+    } else if (state is UpdateDataViewTasksNotExist) {
+      return buildNoTasksMessage();
+    } else if (state is UpdateDataViewTasksFailure) {
+      return Center(child: Text(state.errorMessage));
+    } else {
+      return const Center(child: CircularProgressIndicator());
+    }
+  }
+
+  Widget buildNoTasksMessage() {
+    return Center(
+      child: Text(
+        "No Tasks in this day 😊",
+        style: AppStyle.styleBold18
+            .copyWith(color: const Color(0xff000000).withOpacity(0.5)),
+      ),
+    );
+  }
+
+  Widget buildTasksList(List<TaskModel> tasks) {
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (context, index) => BlocProvider(
+        create: (context) => TaskCubit(),
+        child: CustomExpansionTileTaskWidget(
+          taskIndex: index,
           title:
               Helper.formatTimeOfDayToStringfromDateTime(tasks[index].dateTime),
         ),
-      );
-    } else if (state is UpdateDataViewTasksNotExist) {
-      return Center(
-        child: Text(
-          "No Tasks in this day 😊",
-          style: AppStyle.styleBold18
-              .copyWith(color: const Color(0xff000000).withOpacity(0.5)),
-        ),
-      );
-    } else if (state is UpdateDataViewTasksFailure) {
-      return Center(
-        child: Text(state.errorMessage),
-      );
-    } else {
-      // Handle unexpected states
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
-    }
+      ),
+    );
   }
 }
