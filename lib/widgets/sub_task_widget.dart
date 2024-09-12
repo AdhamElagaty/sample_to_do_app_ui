57f572b7a0b5a970e726cf66899a861bc403d145
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-12 17:26:33 +0300

    Update SubTask widget with Addin SubTaskCubit provider to mange states and change into statlesswidget

--- lib/widgets/sub_task_widget.dart
+++ lib/widgets/sub_task_widget.dart
@@ -1,56 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sample_to_do_app_ui/cubits/sub_task_cubit/sub_task_cubit.dart';
+import 'package:sample_to_do_app_ui/cubits/sub_task_cubit/sub_task_state.dart';
+import 'package:sample_to_do_app_ui/cubits/task_cubit/task_cubit.dart';
 import 'package:sample_to_do_app_ui/utils/app_style.dart';
 
-class SubTaskWidget extends StatefulWidget {
+class SubTaskWidget extends StatelessWidget {
   const SubTaskWidget({
     super.key,
-    required this.onTap,
+    required this.subTaskIndex,
   });
 
-  final void Function()? onTap;
+  final int subTaskIndex;
 
-  @override
-  State<SubTaskWidget> createState() => _SubTaskWidgetState();
-}
-
-class _SubTaskWidgetState extends State<SubTaskWidget> {
-  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        onChange();
+    BlocProvider.of<SubTaskCubit>(context).subTaskModel =
+        BlocProvider.of<TaskCubit>(context)
+            .taskModel!
+            .subTasksModel![subTaskIndex];
+    final subTaskCubit = BlocProvider.of<SubTaskCubit>(context);
+    return BlocConsumer<SubTaskCubit, SubTaskState>(
+      listener: (context, state) {
+        if (state is SubTaskCompleted || state is SubTaskNotCompleted) {
+          BlocProvider.of<TaskCubit>(context).changeStateOfTask();
+        }
       },
-      child: Container(
-        width: double.infinity,
-        color: Colors.transparent,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Checkbox(
-              activeColor: const Color(0xff34C759),
-              value: isChecked,
-              onChanged: (value) {
-                onChange();
-              },
+      builder: (context, state) {
+        return GestureDetector(
+          onTap: subTaskCubit.updateSubTaskCompletion,
+          child: Container(
+            width: double.infinity,
+            color: Colors.transparent,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Checkbox(
+                  activeColor: const Color(0xff34C759),
+                  value: subTaskCubit.subTaskModel!.isCompleted,
+                  onChanged: (value) {
+                    subTaskCubit.updateSubTaskCompletion();
+                  },
+                ),
+                Text(
+                  subTaskCubit.subTaskModel!.title,
+                  style: subTaskCubit.subTaskModel!.isCompleted
+                      ? AppStyle.styleRegular16.copyWith(
+                          decoration: TextDecoration.lineThrough,
+                        )
+                      : AppStyle.styleRegular16.copyWith(color: Colors.black),
+                ),
+              ],
             ),
-            Text(
-              "Cold shower",
-              style: isChecked
-                  ? AppStyle.styleRegular16.copyWith(
-                      decoration: TextDecoration.lineThrough,
-                    )
-                  : AppStyle.styleRegular16.copyWith(color: Colors.black),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
-  }
-
-  void onChange() {
-    widget.onTap;
-    isChecked = !isChecked;
-    setState(() {});
   }
 }
