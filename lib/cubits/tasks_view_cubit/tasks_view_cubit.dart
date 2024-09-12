7c725a204e831dde71c7fa309470ce027cd36e69
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-12 22:26:51 +0300

    Update on TasksViewCubit with adding logic and mange state of delete Task from Tasks List

--- lib/cubits/tasks_view_cubit/tasks_view_cubit.dart
+++ lib/cubits/tasks_view_cubit/tasks_view_cubit.dart
@@ -1,9 +1,13 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sample_to_do_app_ui/cubits/tasks_view_cubit/tasks_view_state.dart';
 import 'package:sample_to_do_app_ui/database/repository/task_repository.dart';
 import 'package:sample_to_do_app_ui/models/task_model.dart';
+import 'package:sample_to_do_app_ui/utils/app_style.dart';
 import 'package:sample_to_do_app_ui/utils/view_date_picker.dart';
+import 'package:sample_to_do_app_ui/utils/view_dialog.dart';
 
 class TasksViewCubit extends Cubit<TasksViewState> {
   TasksViewCubit() : super(TasksViewStateInitial());
@@ -46,5 +50,32 @@ class TasksViewCubit extends Cubit<TasksViewState> {
     } else {
       return 0;
     }
+  }
+
+  Future<void> deleteTask(int taskIndex) async {
+    TaskModel task = tasksModel![taskIndex];
+    try {
+      await TaskRepository().deleteTaskWithSubTasks(task.id!);
+      tasksModel!.removeAt(taskIndex);
+      emit(UpdateDataViewTasksSuccess());
+    } on Exception catch (e) {
+      log(e.toString());
+    }
+  }
+
+  Future<void> deleteTaskWithDialog(BuildContext context, int taskIndex) async {
+    viewCustomDialog(
+      context: context,
+      title: "Delete Task",
+      content: const Text(
+        "Do You Want Delete Task 🤔",
+        style: AppStyle.styleRegular18,
+      ),
+      cancelButtonText: "Cancel",
+      confirmButtonText: "Delete",
+      onConfirm: () async {
+        deleteTask(taskIndex);
+      },
+    );
   }
 }
--- lib/cubits/tasks_view_cubit/tasks_view_state.dart
+++ lib/cubits/tasks_view_cubit/tasks_view_state.dart
@@ -2,14 +2,6 @@ sealed class TasksViewState {}
 
 final class TasksViewStateInitial extends TasksViewState {}
 
-final class TasksViewSuccess extends TasksViewState {}
-
-final class TasksViewFailure extends TasksViewState {
-  final String errorMessage;
-
-  TasksViewFailure({required this.errorMessage});
-}
-
 final class UpdateDateViewTasksSuccess extends TasksViewState {}
 
 final class UpdateDataViewTasksSuccess extends TasksViewState {}
